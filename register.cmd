fc4c1452b9c40e48e9c5a7c4445d38905e1b8b82
valex <valex@localhost>
2009-05-18 08:12:18 +0000

    * Register the filter after build * x64 module renamed

--- register.cmd
+++ register.cmd
@@ -2,7 +2,7 @@ rem @echo off
 echo ------------------------
 echo Register %1
 set filter=%1\spdifer.ax
-if not exist "%filter%" set filter=%1\ac3filter64.ax
+if not exist "%filter%" set filter=%1\spdifer64.ax
 
 regsvr32 /s /u "%filter%"
 regsvr32 /s "%filter%"
