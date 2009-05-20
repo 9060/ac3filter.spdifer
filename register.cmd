3e40346e65ed51fc62eaa9457c4485b0d78a44e3
valex <valex@localhost>
2009-05-20 06:40:56 +0000

    * Report registering errors

--- register.cmd
+++ register.cmd
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 echo ------------------------
 echo Register %1
 set filter=%1\spdifer.ax
@@ -6,3 +6,12 @@ if not exist "%filter%" set filter=%1\spdifer64.ax
 
 regsvr32 /s /u "%filter%"
 regsvr32 /s "%filter%"
+
+if errorlevel 1 goto err
+goto end
+
+:err
+echo Error! Cannot register %filter%.
+fail 2>nul
+
+:end
